d0cd88c53d198649adb8b9e269caea454609efe2
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-08-30 12:55:40 -0500

    Initial Commit

---
+++ XamarinTesting/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace XamarinTesting
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtNumber;
+
+        TextView txtNumber2;
+
+        int number = 20;
+        int number2 = 20;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) => txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) => txtNumber.Text = (--number).ToString();
+
+            txtNumber2 = FindViewById<TextView>(Resource.Id.txtNumber2);
+
+            FindViewById<Button>(Resource.Id.btnIncrement2).Click += (o, e) => txtNumber2.Text = (++number2).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement2).Click += (o, e) => txtNumber2.Text = (--number2).ToString();
+
+        }
+    }
+}
---
+++ XamarinTesting/obj/Debug/MonoAndroid81/android/src/md5b659eafc3bbbca7195caaed33eaadda2/MainActivity.java
@@ -0,0 +1,47 @@
+package md5b659eafc3bbbca7195caaed33eaadda2;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("XamarinTesting.MainActivity, XamarinTesting", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XamarinTesting.MainActivity, XamarinTesting", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
